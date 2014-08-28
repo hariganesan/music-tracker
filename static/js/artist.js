f518da1066a8452069c5a4cb60f3ce9af4d7ebb6
hariganesan <hvganesan@gmail.com>
2014-08-28 00:12:00 -0400

    Basic functionality.

--- static/js/artist.js
+++ static/js/artist.js
@@ -1,30 +1,51 @@
 // artist.js
 
-app = angular.module('artistApp', [])
+var app = angular.module('artistApp', ['ui']);
 
 app.controller('ArtistController', ['$scope', '$http', '$templateCache', 
                function($scope, $http, $templateCache) {
   $scope.artists = [];
   $scope.albums = [];
-  $scope.results = [{name: 'Jack White', id: "4FZ3j1oH43e7cukCALsCwf"}];
+  $scope.results = [];
 
-  $scope.addArtist = function(artist, id) {
-    $scope.artists.push({name:artist, enabled: true});
-    $scope.artistText = '';
-    $scope.fetchAlbums(artist, id);
+  $scope.addArtist = function(artistName, id) {
+    $scope.artists.push({name:artistName, id:id, enabled: true});
+    $scope.searchText = '';
+    $scope.fetchAlbums(artistName, id);
+    $scope.results = [];
   }
 
   $scope.addArtistEnter = function() {
     if ($scope.results.length > 0) {
-      $scope.addArtist($scope.results[0])
+      $scope.addArtist($scope.results[0].name, $scope.results[0].id);
     }
   };
 
   $scope.addArtistClick = function(result) {
     $scope.addArtist(result.name, result.id);
-    $scope.results = [];
-    $scope.$broadcast('newItemAdded');
+    $scope.$broadcast('focusSearch');
   };
+
+  $scope.checkSearch = function() {
+    $scope.results = [];
+
+    if ($scope.searchText && $scope.searchText.length > 2)
+      $scope.fetchArtists();
+  }
+
+  $scope.toggleArtist = function(artist) {
+    artist.enabled = !artist.enabled;
+
+    angular.forEach($scope.albums, function(album) {
+      if (album.artistID === artist.id) {
+        album.enabled = artist.enabled;
+      }
+    });
+  }
+
+  $scope.isEnabled = function(item) {
+    return item.enabled;
+  }
 
   $scope.enabledCount = function() {
     var count = 0;
@@ -48,15 +69,18 @@ app.controller('ArtistController', ['$scope', '$http', '$templateCache',
   $scope.clearArtists = function() {
     $scope.artists = [];
     $scope.albums = [];
+    $scope.$broadcast('focusSearch');
   };
 
   // AJAX stuff
 
-  $scope.fetchArtists = function(artist) {
-    $http({method: 'GET', url: "https://api.spotify.com/v1/search?q="+artist+"&type=artist&limit=3", cache: $templateCache}).
+  $scope.fetchArtists = function() {
+    artistName = $scope.searchText;
+
+    $http({method: 'GET', url: "https://api.spotify.com/v1/search?q="+artistName+"&type=artist&limit=3", cache: $templateCache}).
       success(function(data, status) {
-        if (data.items) {
-          angular.forEach(data.items, function(item) {
+        if (data.artists.items) {
+          angular.forEach(data.artists.items, function(item) {
             $scope.results.push({
               name: item.name,
               id: item.id
@@ -70,18 +94,21 @@ app.controller('ArtistController', ['$scope', '$http', '$templateCache',
     });
   };
 
-  $scope.fetchAlbums = function(artist, id) {
+  $scope.fetchAlbums = function(artistName, id) {
     $http({method: 'GET', url: "https://api.spotify.com/v1/artists/"+id+"/albums?album_type=album", cache: $templateCache}).
       success(function(data, status) {
         if (data.items) {
-          angular.forEach(data.items, function(item, idx) {
+          angular.forEach(data.items, function(item) {
             $scope.albums.push({
-              artist: artist,
+              artist: artistName,
+              artistID: id,
               name: item.name,
-              year: "????"
+              year: "????",
+              id: $scope.albums.length,
+              enabled: true
             });
 
-            $scope.fetchAlbumDate(item.id, idx);
+            $scope.fetchAlbumDate(item.id, $scope.albums.length-1);
           });
         }
       }).
